4baf64c13c40bf01b54ae5b6e8672a34426b56f9
kan <root.kan.ss@gmail.com>
2022-04-08 14:12:05 -0400

    modelo catalago creado

--- src/catalogo/models.py
+++ src/catalogo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Oferta(models.Model):
+    id = models.AutoField(primary_key=True)
+    descuento = models.IntegerField(verbose_name="descuento", blank=False, default=10)
+    fecha_inicio = models.DateField(verbose_name="fecha inicio", blank=False, null=False)
+    fecha_fin = models.DateField(verbose_name="fecha fin", blank=False, null=False)
+    es_activa = models.BooleanField(default=True)
