8130849790ea08b9954aff98975d88b88a38e619
kan <root.kan.ss@gmail.com>
2022-04-09 15:24:57 -0400

    añadido metodo para comprobar si existe alguna oferta actualmente

--- src/catalogo/models.py
+++ src/catalogo/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 class Oferta(models.Model):
     id = models.AutoField(primary_key=True)
-    descuento = models.IntegerField(verbose_name="descuento", blank=False, default=10)
-    fecha_inicio = models.DateField(verbose_name="fecha inicio", blank=False, null=False)
-    fecha_fin = models.DateField(verbose_name="fecha fin", blank=False, null=False)
-    es_activa = models.BooleanField(default=True)
+    descuento = models.IntegerField(verbose_name="descuento", default=10)
+    fecha_inicio = models.DateField(verbose_name="fecha inicio")
+    fecha_fin = models.DateField(verbose_name="fecha fin")
+
+    def oferta_valida(self, fecha_actual):
+        if self.fecha_inicio <= fecha_actual <= self.fecha_fin:
+            return True
