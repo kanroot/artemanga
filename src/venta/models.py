8685064205c7262d6d6c73e43c3b679d1c33d73e
kan <root.kan.ss@gmail.com>
2022-04-09 17:17:34 -0400

    se establecio una conexion entre venta y producto (por diseño una venta puede tener mas de un producto y un mismo producto puede estar en mas de una venta)

--- src/venta/models.py
+++ src/venta/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from despacho.models import Despacho
+from inventario.models import Producto
 from .tipo_enum.estado_venta import ESTADO_VENTA_CHOICES, EstadoVenta
 
 
@@ -8,6 +9,6 @@ class Venta(models.Model):
     total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="total")
     fecha_venta = models.DateField(verbose_name="fecha venta")
     estado = models.PositiveSmallIntegerField(choices=ESTADO_VENTA_CHOICES, default=EstadoVenta.PENDIENTE.value)
-    productos = models.ManyToManyField('Producto', through='VentaProducto')
+    productos = models.ManyToManyField(Producto)
     # conexiones
     despacho = models.OneToOneField(Despacho, on_delete=models.CASCADE)
