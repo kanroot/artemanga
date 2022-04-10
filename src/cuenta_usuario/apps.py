2b2df0fff68b7440d894308a8f70488c3057b15f
kan <root.kan.ss@gmail.com>
2022-04-10 11:22:18 -0400

    fix tipo

--- src/cuenta_usuario/apps.py
+++ src/cuenta_usuario/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CuentausuarioConfig(AppConfig):
+class CuentaUsuarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cuenta_usuario'
