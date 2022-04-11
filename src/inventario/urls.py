636d00c5dc3980e32d4794213327de31b7a04b86
kan <root.kan.ss@gmail.com>
2022-04-11 18:30:41 -0400

    plantilla estandar del listado de registro acceso "http://127.0.0.1:8000/inventario/url-respectiva-del-crud"

--- src/inventario/urls.py
+++ src/inventario/urls.py
@@ -2,5 +2,11 @@ from django.urls import re_path
 from inventario import views
 
 urlpatterns = [
-    re_path('registro', views.index, name='crud_autor.html'),
-    ]
+    re_path('crud-autor', views.crud_autor, name='listado.html'),
+    re_path('crud-genero', views.crud_genero, name='listado.html'),
+    re_path('crud-pais', views.crud_pais, name='listado.html'),
+    re_path('crud-editorial', views.crud_editorial, name='listado.html'),
+    re_path('crud-otros-autores', views.crud_otros_autores, name='listado.html'),
+    re_path('crud-iva', views.crud_iva, name='listado.html'),
+    re_path('crud-producto', views.crud_producto, name='listado.html'),
+]
--- src/inventario/views.py
+++ src/inventario/views.py
@@ -3,29 +3,109 @@ from inventario.models import Autor, Genero, Pais, Editorial, OtrosAutores, IVA,
 
 
 # Create your views here.
-def index(request):
-    autor = Autor.objects.all()
-    genero = Genero.objects.all()
-    pais = Pais.objects.all()
-    editorial = Editorial.objects.all()
-    otros_autores = OtrosAutores.objects.all()
-    iva = IVA.objects.all()
-    producto = Producto.objects.all()
-    return render(request, 'crud_autor.html', {"autores": autor})
-
 
 def crud_autor(request):
-    autor = Autor.objects.all()
     contexto = {
         'cabeceras':
             [
                 'Nombre',
                 'Apellido',
-                'Es_activo'
+                'Es_activo',
+                'Modificar',
+                'Eliminar'
             ],
-        'filas':
-            [
-
-            ]
+        'filas': [[a.nombre, a.apellido, a.es_activo] for a in Autor.objects.all()],
+        'titulo': 'Autores'
     }
-    return render(request, 'crud_autor.html', {"autores": autor})
+    return render(request, 'listado.html', contexto)
+
+
+def crud_genero(request):
+    contexto = {
+        'cabeceras':
+            [
+                'Nombre',
+                'Modificar',
+                'Eliminar'
+            ],
+        'filas': [[a.nombre] for a in Genero.objects.all()],
+        'titulo': 'Generos'
+    }
+    return render(request, 'listado.html', contexto)
+
+
+def crud_pais(request):
+    contexto = {
+        'cabeceras':
+            [
+                'ID',
+                'Nombre'
+            ],
+        'filas': [[a.id, a.nombre] for a in Pais.objects.all()],
+        'titulo': 'Paises'
+    }
+    return render(request, 'listado.html', contexto)
+
+
+def crud_editorial(request):
+    contexto = {
+        'cabeceras':
+            [
+                'ID',
+                'Nombre',
+                'Pais'
+            ],
+        'filas': [[a.id, a.nombre, a.pais] for a in Editorial.objects.all()],
+        'titulo': 'Editoriales'
+    }
+    return render(request, 'listado.html', contexto)
+
+
+def crud_otros_autores(request):
+    contexto = {
+        'cabeceras':
+            [
+                'ID',
+                'Nombre',
+                'Cargo'
+            ],
+        'filas': [[a.id, a.nombre, a.cargo] for a in OtrosAutores.objects.all()],
+        'titulo': 'Otros autores'
+    }
+    return render(request, 'listado.html', contexto)
+
+
+def crud_iva(request):
+    contexto = {
+        'cabeceras':
+            [
+                'IVA'
+            ],
+        'filas': [[a.iva] for a in IVA.objects.all()],
+        'titulo': 'IVA'
+    }
+    return render(request, 'listado.html', contexto)
+
+
+def crud_producto(request):
+    contexto = {
+        'cabeceras':
+            [
+                'ISBN',
+                'Titulo español',
+                'Titulo japones',
+                'Stock',
+                'Precio',
+                'Descripcion',
+                'Numero de paginas',
+                'Es color',
+                'Fecha de publicacion',
+                'Esta publicado',
+                'Es destacado'
+
+            ],
+        'filas': [[a.isbn, a.titulo_es, a.titulo_jp, a.stock, a.precio, a.descripcion, a.numero_paginas, a.es_color,
+                   a.fecha_publicacion, a.esta_publicado, a.es_destacado] for a in Producto.objects.all()],
+        'titulo': 'Productos'
+    }
+    return render(request, 'listado.html', contexto)
