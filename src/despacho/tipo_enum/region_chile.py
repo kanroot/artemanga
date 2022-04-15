81d40ed9b2f7e2d9fdc78b24b5fb0ad85200162f
kan <root.kan.ss@gmail.com>
2022-04-15 16:44:34 -0400

    modelo textChoice para region añadido

--- src/despacho/tipo_enum/region_chile.py
+++ src/despacho/tipo_enum/region_chile.py
@@ -1,23 +1,20 @@
-from enum import StrEnum
+from django.db import models
 
 
-class RegionChile(StrEnum):
-    TARAPACA = 'Region de Tarapacá'
-    ANTOFAGASTA = 'Region de Antofagasta'
-    ATACAMA = 'Region de Atacama'
-    COQUIMBO = 'Region de Coquimbo'
-    VALPARAISO = 'Region de Valparaíso'
-    OHIGGINS = 'Región del Libertador General Bernardo O’Higgins'
-    MAULE = 'Región del Maule'
-    BIOBIO = 'Región del Biobío'
-    ARAUCANIA = 'Región de la Araucanía'
-    LAGOS = 'Región de los Lagos'
-    IBANEZ = 'Región Aysén del General Carlos Ibáñez del Campo'
-    MAGALLANES = 'Región de Magallanes y de la Antártica Chilena'
-    METROPOLITANA = 'Región Metropolitana de Santiago'
-    RIOS = 'Región de los Ríos'
-    ARICA_Y_PARINACOTA = 'Región de Arica y Parinacota'
-    NUBLE = 'Región de Ñuble'
-
-
-REGION_CHILE_CHOISE = [[regionDes.upper()] for regionDes in RegionChile]
+class RegionChile(models.TextChoices):
+    TARAPACA = '1', 'Region de Tarapacá'
+    ANTOFAGASTA = '2', 'Region de Antofagasta'
+    ATACAMA = '3', 'Region de Atacama'
+    COQUIMBO = '4', 'Region de Coquimbo'
+    VALPARAISO = '5', 'Region de Valparaíso'
+    OHIGGINS = '6', 'Región del Libertador General Bernardo O’Higgins'
+    MAULE = '7', 'Región del Maule'
+    BIOBIO = '8', 'Región del Biobío'
+    ARAUCANIA = '9', 'Región de la Araucanía'
+    LAGOS = '10', 'Región de los Lagos'
+    IBANEZ = '11', 'Región Aysén del General Carlos Ibáñez del Campo'
+    MAGALLANES = '12', 'Región de Magallanes y de la Antártica Chilena'
+    METROPOLITANA = '13', 'Región Metropolitana de Santiago'
+    RIOS = '14', 'Región de los Ríos'
+    ARICA_Y_PARINACOTA = '15', 'Región de Arica y Parinacota'
+    NUBLE = '16', 'Región de Ñuble'
