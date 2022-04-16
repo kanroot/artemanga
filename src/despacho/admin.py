f533ea77fa0f61554e37aee70dcdbf0dca3f1b88
kan <root.kan.ss@gmail.com>
2022-04-16 13:10:26 -0400

    campos de region, provincia, comuna y direccion añadidos al admin

--- src/despacho/admin.py
+++ src/despacho/admin.py
@@ -3,5 +3,25 @@ from .models import Despacho
 
 
 @admin.register(Despacho)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+
+
+@admin.register(Despacho)
+class ProvinciaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+
+
+@admin.register(Despacho)
+class ComunaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+
+
+@admin.register(Despacho)
+class DireccionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'calle', 'numero', 'departamento', 'piso', 'codigo_postal', 'telefono')
+
+
+@admin.register(Despacho)
 class DespachoAdmin(admin.ModelAdmin):
     list_display = ('id', 'usuario')
