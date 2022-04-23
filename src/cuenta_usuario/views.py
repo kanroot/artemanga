1ceab58a23852591efdfb6f073a333b40a046e8e
kan <root.kan.ss@gmail.com>
2022-04-23 16:32:13 -0400

    bosquejo del formulario de registro

--- src/cuenta_usuario/views.py
+++ src/cuenta_usuario/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from cuenta_usuario.forms import UserRegisterForm
+
 
 # Create your views here.
+class SignUpView(CreateView):
+    template_name = 'cuenta_usuario/templates/registro.html'
+    success_url = reverse_lazy('login')
+    form_class = UserRegisterForm
+    success_message = "Tú perfil ha sido creado con éxito"
