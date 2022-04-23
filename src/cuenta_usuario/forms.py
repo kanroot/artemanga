a5311402da2d7e15e0ecd81c37711e83a656ea42
kan <root.kan.ss@gmail.com>
2022-04-23 16:32:13 -0400

    bosquejo del formulario de registro

---
+++ src/cuenta_usuario/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from cuenta_usuario.models import Usuario
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    primer_nombre = forms.CharField(label='Primer Nombre')
+    segundo_nombre = forms.CharField(label='Segundo Nombre', required=False)
+    primer_apellido = forms.CharField(label='Primer Apellido')
+    segundo_apellido = forms.CharField(label='Segundo Apellido', required=False)
+    email = forms.EmailField(label='Correo Electrónico')
+    sexo = forms.ChoiceField(label='Sexo',
+                             choices=(('M', 'Masculino'), ('F', 'Femenino'), ('O', 'Otro')),
+                             required=False)
+
+    class Meta:
+        model = Usuario
+        fields = ['primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido', 'email', 'sexo',
+                  'password1', 'password2']
+        widgets = {
+            'password1': forms.PasswordInput(),
+            'password2': forms.PasswordInput(),
+            'email': forms.TextInput(
+                attrs={
+                    'type': 'email',
+                    'class': 'validate',
+                }
+            )
+        }
