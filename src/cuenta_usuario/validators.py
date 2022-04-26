3f97f741bf82d0a4fc48df838fb23943f1cd4fea
Gilles <43683714+corp-0@users.noreply.github.com>
2022-04-26 00:25:16 -0400

    Apply suggestions from code review

--- src/cuenta_usuario/validators.py
+++ src/cuenta_usuario/validators.py
@@ -7,6 +7,6 @@ class UsernameValidator(ASCIIUsernameValidator):
     # concuerda con username que tenga a lo menos 3 caracteres, letras, numeros y guiones bajos
     regex = r"^[a-zA-Z0-9.-](?!.* {2})[ \w.-]{2,}$"
     message = (
-        "Ingresa un nick valido. Este campo solo puede ser rellenado con caracteres latinos, "
-        "numeros, guion, guion bajo, puntos, y espacios."
+        "Ingresa un nick válido. Este campo sólo puede ser rellenado con caracteres latinos, "
+        "números, guión, guión bajo, puntos, y espacios."
     )
