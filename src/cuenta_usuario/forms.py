614907b2a0652b23a2f592b991d66ede50c92ff7
kanroot <61951793+kanroot@users.noreply.github.com>
2022-05-02 13:07:10 -0400

    Integraciones inciales (#100)
    * obligatoriedad del email
    
    * integraciones del login, registrar y recuperar contraseña
    
    * actualizar perfil
    
    * email obligatorio
    
    * importacion eliminada
    
    * dependencias no usadas
    
    * campos de primer nombre y primer apellido devueltos
    
    * se integro la opcion de si no estas logeado, no puedes ir a la opcion de editar perfil, te manda al login

--- src/cuenta_usuario/forms.py
+++ src/cuenta_usuario/forms.py
@@ -1,5 +1,5 @@
-
 from django import forms
+
 from .models import Usuario
 from django.contrib.auth.forms import UserCreationForm
 
@@ -9,16 +9,13 @@ class RegistroUsuarioForm(UserCreationForm):
         label='Sexo',
         choices=((4, 'No deseo responder'), (1, 'Masculino'), (2, 'Femenino'), (3, 'Otro'))
     )
+    email = forms.EmailField(
+        label='Correo electrónico',
+        required=True,
+    )
 
     class Meta:
         model = Usuario
         fields = [
             'username', 'primer_nombre', 'segundo_nombre', 'primer_apellido', 'segundo_apellido', 'email', 'sexo',
             'password1', 'password2']
-
-
-
-
-
-
-
--- src/cuenta_usuario/urls.py
+++ src/cuenta_usuario/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import InicioSesionView, ReinicioContrasenaView, RegistroUsuarioView, ActualizarUsuarioView, CerrarSesionView
+from .views import InicioSesionView, ReinicioContrasenaView, RegistroUsuarioView, CerrarSesionView, \
+    ActualizarUsuarioView
 
 urlpatterns = [
     path('reset-password/', ReinicioContrasenaView.as_view(), name='reset-password'),
     path('crear-perfil/', RegistroUsuarioView.as_view(), name='crear-perfil'),
-    path('editar-perfil/<pk>/', ActualizarUsuarioView.as_view(), name='editar-perfil'),
+    path('editar-perfil/', ActualizarUsuarioView.as_view(), name='editar-perfil'),
     path('login/', InicioSesionView.as_view(), name='login'),
     path('logout/', CerrarSesionView.as_view(), name='logout'),
 ]
