c18cd4691efe7303f286fdfa27c27a2672cf975f
kan <root.kan.ss@gmail.com>
2022-05-02 21:46:27 -0400

    restricciones

--- src/inventario/vistas_modelos/venta.py
+++ src/inventario/vistas_modelos/venta.py
@@ -1,12 +1,14 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, UpdateView
 from cuenta_usuario.restriccion import VistaRestringida
+from cuenta_usuario.enums.opciones import TipoUsuario
 from artemangaweb.mixins import MensajeResultadoFormMixin
 from venta.models import Venta
 from venta.tipo_enum.estado_venta import EstadoVenta
 
 
 class VentasLisView(VistaRestringida, ListView):
+    usuarios_permitidos = [TipoUsuario.VENTAS]
     model = Venta
     template_name = 'ventas/listado_ventas.html'
     queryset = Venta.objects.filter(estado=EstadoVenta.PENDIENTE.value)
@@ -16,6 +18,7 @@ class VentasLisView(VistaRestringida, ListView):
 
 
 class VentasAprobadasLisView(VistaRestringida, ListView):
+    usuarios_permitidos = [TipoUsuario.VENTAS]
     model = Venta
     template_name = 'ventas/listado_ventas.html'
     queryset = Venta.objects.filter(estado=EstadoVenta.APROBADA.value)
@@ -25,6 +28,7 @@ class VentasAprobadasLisView(VistaRestringida, ListView):
 
 
 class VentaUpdateView(VistaRestringida, MensajeResultadoFormMixin, UpdateView):
+    usuarios_permitidos = [TipoUsuario.VENTAS]
     model = Venta
     template_name = 'CRUD/form_generico.html'
     success_url = reverse_lazy('ventas-validar')
