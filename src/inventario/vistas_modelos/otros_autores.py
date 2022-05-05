f818a1591c355788da7c493786daae13a248d9e9
kan <root.kan.ss@gmail.com>
2022-05-04 21:45:18 -0400

    otros autores template estandar

--- src/inventario/vistas_modelos/otros_autores.py
+++ src/inventario/vistas_modelos/otros_autores.py
@@ -3,15 +3,21 @@ from django.views.generic import ListView
 
 from inventario.models import OtrosAutores
 from .vistas_genericas import CrearGenerico, ActualizarGenerico, EliminarGenerico
+from artemangaweb.mixins import MensajeResultadoFormMixin, TituloPaginaMixin
+from cuenta_usuario.enums.opciones import TipoUsuario
+from cuenta_usuario.restriccion import VistaRestringida
 
 URL_EXITO = reverse_lazy('listado-otro-autor')
 
 
-class OtrosAutoresListView(ListView):
+class OtrosAutoresListView(TituloPaginaMixin, VistaRestringida,ListView):
+    titulo_pagina = 'Listado de Generos'
+    usuarios_permitidos = [TipoUsuario.ADMINISTRADOR, TipoUsuario.BODEGA]
     model = OtrosAutores
     template_name = 'administración/CRUD/listado_otro_autor.html'
     paginate_by = 10
     ordering = ['id']
+    context_object_name = 'otros_autores'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
