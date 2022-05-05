6801c052271114c091aa7b35193aa99ca6c1c3f1
kan <root.kan.ss@gmail.com>
2022-05-04 21:50:21 -0400

    autor actualizado

--- src/inventario/vistas_modelos/autor.py
+++ src/inventario/vistas_modelos/autor.py
@@ -4,11 +4,12 @@ from inventario.models import Autor
 from cuenta_usuario.enums.opciones import TipoUsuario
 from .vistas_genericas import CrearGenerico, ActualizarGenerico, EliminarGenerico
 from artemangaweb.mixins import MensajeResultadoFormMixin, TituloPaginaMixin
+from cuenta_usuario.restriccion import VistaRestringida
 
 EXITO_URL = reverse_lazy('listado-autor')
 
 
-class AutorListView(TituloPaginaMixin,
+class AutorListView(TituloPaginaMixin, MensajeResultadoFormMixin, VistaRestringida,
                     ListView):
     model = Autor
     context_object_name = 'Autores'
@@ -30,16 +31,27 @@ class AutorListView(TituloPaginaMixin,
         return context
 
 
-class AutorCreateView(CrearGenerico):
+class AutorCreateView(TituloPaginaMixin, MensajeResultadoFormMixin, VistaRestringida,CrearGenerico):
+    titulo_pagina = "Crear autor"
+    usuarios_permitidos = [TipoUsuario.ADMINISTRADOR, TipoUsuario.BODEGA]
+    mensaje_error = "No se pudo crear el autor"
+    mensaje_exito = "Se creó el autor"
     model = Autor
     success_url = EXITO_URL
 
 
-class AutorUpdateView(ActualizarGenerico):
+class AutorUpdateView(TituloPaginaMixin, MensajeResultadoFormMixin, VistaRestringida,ActualizarGenerico):
+    titulo_pagina = "Actualizar autor"
+    usuarios_permitidos = [TipoUsuario.ADMINISTRADOR, TipoUsuario.BODEGA]
+    mensaje_error = "No se pudo actualizar el autor"
+    mensaje_exito = "Se actualizó el autor"
     model = Autor
     success_url = EXITO_URL
 
 
-class AutorDeleteView(EliminarGenerico):
+class AutorDeleteView(TituloPaginaMixin, MensajeResultadoFormMixin, VistaRestringida,EliminarGenerico):
+    titulo_pagina = "Eliminar autor"
+    usuarios_permitidos = [TipoUsuario.ADMINISTRADOR, TipoUsuario.BODEGA]
+    mensaje_error = "No se pudo eliminar el autor"
     model = Autor
     success_url = EXITO_URL
