57a4609a7b13034fec1ef290d707d4ab6be277d1
Gilles <43683714+corp-0@users.noreply.github.com>
2022-05-07 15:43:29 -0400

    feat: agrega vistas de catálogo filtradas por categoría y editorial (#124)
    * feat: agrega views de catalogo con filtro (categoría o editorial)
    
    * fix: hace título de cards de mangas más pequeño
    
    * fix: corrige url de agregar al carrito para que funcione en todas partes
    
    * feat: agrega sección de productos random al home
    
    * chore: agrega django-extensions a las dependencias
    
    Co-authored-by: kanroot <61951793+kanroot@users.noreply.github.com>

--- src/catalogo/urls.py
+++ src/catalogo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Home, VerCarritoView, ActualizarCarritoView, AgregarProductoCarritoView, DetalleProducto
 
+from .views import (Home, VerCarritoView, ActualizarCarritoView,
+                    AgregarProductoCarritoView, ProductosPorCategoriaView, ProductosPorEditorialView,
+                    DetalleProducto)
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
@@ -9,5 +11,7 @@ urlpatterns = [
     path('ver-carrito', VerCarritoView.as_view(), name='ver-carrito'),
     path('actualizar-carrito', ActualizarCarritoView.as_view(), name='ajax-actualizar-carrito'),
     path('agregar-producto-carrito', AgregarProductoCarritoView.as_view(), name='ajax-agregar-producto-carrito'),
+    path('categoria/<id>', ProductosPorCategoriaView.as_view(), name='productos-por-categoria'),
+    path('editorial/<id>', ProductosPorEditorialView.as_view(), name='productos-por-editorial'),
     path('detalle-pro/<pk>',DetalleProducto.as_view(),name='detalle-producto'),
 ]
--- src/catalogo/views.py
+++ src/catalogo/views.py
@@ -1,18 +1,22 @@
-
 from django.http import HttpResponse
-from django.views.generic import TemplateView, View, DetailView
-from inventario.models import Producto
+from django.views.generic import TemplateView, View, ListView, DetailView
 
 from catalogo.carrito.models import Carrito, EntradaCarrito
+from inventario.models import Producto, Genero, Editorial
 
 
-
-# Create your views here.
 class Home(TemplateView):
     template_name= "web/index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['randoms'] = Producto.objects.order_by('?')[:5]
+        return context
+
+
 class VerCarritoView(TemplateView):
     template_name = 'web/carrito.html'
+
 
 class ActualizarCarritoView(View):
     def post(self, request):
@@ -32,6 +36,7 @@ class ActualizarCarritoView(View):
         carrito.guardar(request.session)
         return HttpResponse(carrito)
 
+
 class AgregarProductoCarritoView(View):
     def post(self, request):
         carrito: Carrito = Carrito.deserializar(request.session['carrito'])
@@ -41,6 +46,30 @@ class AgregarProductoCarritoView(View):
         carrito.agregar_producto(producto)
         carrito.guardar(request.session)
         return HttpResponse(carrito)
+
+
+class ProductosPorCategoriaView(ListView):
+    template_name = 'web/catalogo_filtrado.html'
+    context_object_name = 'productos'
+    extra_context = {}
+
+    def get_queryset(self):
+        genero = Genero.objects.get(id=self.kwargs['id'])
+        self.extra_context['filtro_desc'] = 'categoría'
+        self.extra_context['filtro'] = genero
+        return Producto.objects.filter(esta_publicado=True).filter(genero=genero)
+
+
+class ProductosPorEditorialView(ListView):
+    template_name = 'web/catalogo_filtrado.html'
+    extra_context = {'filtro_desc': 'editorial'}
+    context_object_name = 'productos'
+
+    def get_queryset(self):
+        editorial = Editorial.objects.get(id=self.kwargs['id'])
+        self.extra_context['filtro_desc'] = 'editorial'
+        self.extra_context['filtro'] = editorial
+        return Producto.objects.filter(esta_publicado=True).filter(editorial=editorial)
 
 class DetalleProducto(DetailView):
     template_name= "web/detalle_pro.html"
