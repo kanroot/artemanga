dc62575146899a897bc178e95ad3bad4fcb3b8f8
kanroot <61951793+kanroot@users.noreply.github.com>
2022-05-30 11:18:46 -0400

    editar el despacho junto con validar la venta (#172)
    * editar el despacho junto con validar la venta
    
    * codacy

--- src/inventario/forms.py
+++ src/inventario/forms.py
@@ -1,3 +1,4 @@
+from venta.models import Despacho, Venta
 from .models import Producto, Genero
 from django import forms
 
@@ -26,3 +27,21 @@ class ActualizarProductoVentasForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ['es_nuevo', 'es_destacado', 'esta_publicado', 'precio']
+
+
+class ActualizarVentaForm(forms.ModelForm):
+    class Meta:
+        model = Venta
+        fields = ['estado', 'imagen_deposito', 'boleta']
+
+        imagen_deposito = forms.ImageField(required=False, widget=forms.FileInput)
+        boleta = forms.FileField(required=False, widget=forms.ClearableFileInput(attrs={'multiple': False}))
+
+
+class DespachoForm(forms.ModelForm):
+    class Meta:
+        model = Despacho
+        fields = ['estado', 'codigo_seguimiento']
+        labels = {
+            'estado': 'Estado del despacho'
+        }
--- src/inventario/vistas_modelos/venta.py
+++ src/inventario/vistas_modelos/venta.py
@@ -1,10 +1,12 @@
-from django.urls import reverse_lazy
-
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy, reverse
 from artemangaweb.mixins import VistaRestringidaMixin
 from cuenta_usuario.enums.opciones import TipoUsuario
 from venta.enums.opciones import EstadoVenta
-from venta.models import Venta
+from venta.models import Venta, Despacho
 from .vistas_genericas import ActualizarGenericoView, ListaGenericaView
+from inventario.forms import DespachoForm, ActualizarVentaForm
 
 
 class VentasPendientesLisView(VistaRestringidaMixin, ListaGenericaView):
@@ -38,6 +40,32 @@ class VentasAprobadasLisView(VistaRestringidaMixin, ListaGenericaView):
 class VentaUpdateView(VistaRestringidaMixin, ActualizarGenericoView):
     usuarios_permitidos = [TipoUsuario.ADMINISTRADOR, TipoUsuario.VENTAS]
     model = Venta
-    template_name = 'administración/CRUD/form_generico.html'
+    template_name = 'administración/CRUD/validar_venta.html'
     success_url = reverse_lazy('ventas-validar')
-    fields = ['estado', 'boleta']
+    form_class = ActualizarVentaForm
+    second_form_class = DespachoForm
+    fields = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        venta = get_object_or_404(Venta, pk=self.kwargs['pk'])
+        context['venta'] = venta
+        context['form'] = self.form_class(instance=venta)
+        context['form2'] = self.second_form_class(instance=venta.despacho)
+        context['titulo_pagina_dos'] = get_object_or_404(Despacho, venta__pk=self.kwargs['pk']).direccion
+        return context
+
+    def post(self, request, *args, **kwargs):
+        pk = self.kwargs['pk']
+        venta = get_object_or_404(Venta, pk=pk)
+        venta_form = ActualizarVentaForm(instance=venta, data=request.POST, files=request.FILES or None)
+        despacho = get_object_or_404(Despacho, venta__pk=pk)
+        despacho_form = DespachoForm(instance=despacho, data=request.POST)
+
+        if 'submit_venta' in request.POST:
+            if venta_form.is_bound and venta_form.is_valid():
+                venta_form.save()
+        elif 'submit_despacho' in request.POST:
+            if despacho_form.is_bound and despacho_form.is_valid():
+                despacho_form.save()
+        return HttpResponseRedirect(reverse('dashboard-ventas'))
